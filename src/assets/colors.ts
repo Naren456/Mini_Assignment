2628881751ac2d983a4a289390c3c018755ae75c
Naren456 <narendrase666@gmail.com>
2026-01-08 18:31:39 +0530

    global colors

---
+++ src/assets/colors.ts
@@ -0,0 +1,15 @@
+export const Colors = {
+  background: '#2D2E2F',
+  primary: '#007AFF',
+  secondary: '#FFFFFF',
+  textPrimary: '#FFFFFF',
+  textSecondary: '#4B4B4B',
+  link: '#2398FE',
+  card: '#1C1C1E',
+  border: '#FFFFFF',
+  borderEmpty: '#FFFFFF33',
+  buttonDisabled: '#FFFFFF0A',
+  black: '#000000',
+};
+
+export default Colors;
